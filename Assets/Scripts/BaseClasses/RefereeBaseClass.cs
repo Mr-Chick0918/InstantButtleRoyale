307f89c76e90cdea118cde71fa707489d648028f
Mr-Chick0918 <contenerezza0479@gmail.com>
2020-09-16 15:36:51 +0900

    First Commit
    MrChick Implemented the test of "Tap100times".

---
+++ Assets/Scripts/BaseClasses/RefereeBaseClass.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RefereeBaseClass : MonoBehaviour
+{
+    public enum RefereeingStyle
+    {
+        FOR_OVER  = 0,
+        FOR_CLEAR = 1,
+    }
+
+    /// <summary>
+    /// If you want the free timing for Refereeing, you should set "false".
+    /// </summary>
+    [SerializeField]
+    bool autoRefereeing = true;
+
+    /// <summary>
+    /// If the game need refereeing for gameover, example the survival, you should set "FOR_OVER".
+    /// </summary>
+    [SerializeField]
+    RefereeingStyle style = RefereeingStyle.FOR_CLEAR;
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        if (autoRefereeing)
+            Refereeing();
+    }
+
+    public void Refereeing()
+    {
+        if(style != RefereeingStyle.FOR_OVER)
+        {
+            if (Judgment())
+                GameClear();
+        }
+        else
+        {
+            if (Judgment())
+                GameOver();
+        }
+    }
+
+    public virtual bool Judgment()
+    {
+        return false;
+    }
+
+    public virtual void GameClear()
+    {
+
+    }
+
+    public virtual void GameOver()
+    {
+
+    }
+}
---
+++ Assets/Scripts/MiniGames/Test_Tap100times/Referee_Test_Tap100times.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Referee_Test_Tap100times : RefereeBaseClass
+{
+    int tapCount = 0;
+
+    public bool isClear = false;
+
+    protected override void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            tapCount++;
+        }
+
+        base.Update();
+    }
+
+    public override bool Judgment()
+    {
+        if (tapCount >= 100)
+            return true;
+        else
+            return false;
+    }
+
+    /// <summary>
+    /// If you feel hard to watch the gameclear, you should commentout this, and release the comments under this function. 
+    /// </summary>
+    public override void GameClear()
+    {
+        isClear = true;
+    }
+
+//    public override void GameClear()
+//    {
+//        Quit();
+//    }
+
+//    void Quit()
+//    {
+//#if UNITY_EDITOR
+//        UnityEditor.EditorApplication.isPlaying = false;
+//#elif UNITY_STANDALONE
+//        UnityEngine.Application.Quit();
+//#endif
+//    }
+}
